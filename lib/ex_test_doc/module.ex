a95f70ca3b2557ac4ba2061c80baaf5634a06519
snate <valle.sebastiano93@gmail.com>
2016-11-27 16:55:49 +0100

    Add tests list to Module structure

--- lib/ex_test_doc/module.ex
+++ lib/ex_test_doc/module.ex
@@ -1,8 +1,9 @@
 defmodule ExTestDoc.Module do
-  defstruct name: nil, lines: nil
+  defstruct name: nil, lines: nil, tests: nil
 
   @type t :: %__MODULE__{
     name: nil | String.t,
-    lines: nil | list()
+    lines: nil | list(),
+    tests: nil | list()
   }
 end
